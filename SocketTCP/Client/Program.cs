0f8776a0e6858f762242b1e4b9d69cf7584c4895
Admin <nguyenleminh.paris13@gmail.com>
2023-03-21 11:31:22 +0700

    Update

--- SocketTCP/Client/Program.cs
+++ SocketTCP/Client/Program.cs
@@ -2,6 +2,11 @@
 using System.Net;
 using System.Net.Sockets;
 
+//Thứ tự công việc bên phía Client
+//1. Thiết lập IPEndpointvà Socket
+//2. Kết nối Socket với IP Endpoint
+//3. Gửi nhận dữ liệu từ Server
+
 namespace Client
 {
     class Program
@@ -9,6 +14,8 @@ namespace Client
         static void Main(string[] args)
         {
             Console.Title = "TCP CLient";
+
+            //Nhập địa chỉ IPAddress và Port
             Console.Write("Server IP address: ");
             var _serverIP = Console.ReadLine();
             var serverIP = IPAddress.Parse(_serverIP);
@@ -17,6 +24,7 @@ namespace Client
             var _serverPort = Console.ReadLine();
             var serverPort = int.Parse(_serverPort);
 
+            //Thiết lập IPEndPoint
             var serverEndpoint = new IPEndPoint(serverIP, serverPort);
             Console.Write("Type the text sent to the server \n");
 
@@ -25,30 +33,32 @@ namespace Client
 
             while (true)
             {
-                // Gui di
+            //Kết nối Socket với IPEndpoint
+                var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(serverEndpoint);
+
+            //Gửi dữ liệu tới Server
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.Write(" #Text >>> ");
                 Console.ResetColor();
                 var text = Console.ReadLine();
 
-                var socket = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
-                socket.Connect(serverEndpoint);
-
                 var sendBuffer = Encoding.ASCII.GetBytes(text);
                 socket.Send(sendBuffer);
-                socket.Shutdown(SocketShutdown.Send);
+                socket.Shutdown(SocketShutdown.Send); //Đóng kết nối, không gửi dữ liệu nữa 
 
 
-                //Nhan ve
+            //Nhận dữ liệu từ Server
                 var length = socket.Receive(receiveBuffer);
                 var result = Encoding.ASCII.GetString(receiveBuffer, 0, length);
-                Array.Clear(receiveBuffer, 0, size);
-
-                socket.Shutdown(SocketShutdown.Receive);
-
-                socket.Close();
                 Console.WriteLine($"Respond from Server <<< {result}");
                 Console.WriteLine("-----------------------------------------");
+                socket.Shutdown(SocketShutdown.Receive); //Đóng kết nối, không nhận dữ liệu nữa
+
+                socket.Close();
+
+            //Xóa bộ đệm
+                Array.Clear(receiveBuffer, 0, size);
             }
         }
     }
--- SocketTCP/Server/Program.cs
+++ SocketTCP/Server/Program.cs
@@ -1,6 +1,13 @@
-﻿using System.Text;
-using System.Net;
-using System.Net.Sockets;
+﻿using System.Text; // Để sử dụng lớp Encoding
+using System.Net; // Để sử dụng lớp IPAddress, IPEndPoint
+using System.Net.Sockets; // Để sử dụng lớp Socket
+
+//Thứ tự công việc bên phía Sever:
+//1. Thiết lập IPEndpoint và Socket
+//2. Kết nối Socket với IP Endpoint
+//3. Lắng nghe từ Client
+//4. Chấp nhận kết nối từ Client
+//5. Gửi nhận dữ liệu từ Client
 
 namespace Server
 {
@@ -10,13 +17,15 @@ namespace Server
         {
             Console.Title = "TCP Server";
 
-            var localIP = IPAddress.Any;
-            var localPort = 1308;
+            //Thiết lập IPEndpoint
+            var localIP = IPAddress.Any; //Chấp nhận tất cả địa chỉ IPAddress
+            var localPort = 1308; // Nghe tất cả các gói tin gửi qua cổng 1308
             var localEndPoint = new IPEndPoint(localIP, localPort);
 
-            var listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            listener.Bind(localEndPoint);
-            listener.Listen(10);
+            //Thiết lập Socket
+            var listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);//Gửi dữ liệu theo giao thức TCP, loại Socket Stream, họ địa chỉ IPv4
+            listener.Bind(localEndPoint); //Kết nối Socket và EndPoint
+            listener.Listen(10); //Lắng nghe từ Client, tối đa 10 request mỗi lần
             Console.WriteLine($"Local socket bind to {localEndPoint}. Waitting for request ....");
 
             var size = 1024;
@@ -24,27 +33,29 @@ namespace Server
 
             while (true)
             {
+            // Chấp nhận kết nối từ Client
                 var socket = listener.Accept();
                 Console.WriteLine($"Accepted connection from {socket.RemoteEndPoint}");
 
+            //Nhận dữ liệu từ Client
                 var length = socket.Receive( receiveBuffer );
                 socket.Shutdown(SocketShutdown.Receive);
                 var text = Encoding.ASCII.GetString( receiveBuffer ,0,length);
                 Console.WriteLine($"Received from Client: {text}");
+                socket.Shutdown(SocketShutdown.Receive); //Đóng kết nối, không nhận dữ liệu nữa
 
+            //Chuyển dữ liệu sang chữ in hoa và gửi lại Client
                 var result = text.ToUpper();    
-
                 var sendBuffer = Encoding.ASCII.GetBytes(result);
-
                 socket.Send(sendBuffer);
                 Console.WriteLine($"Send to Client: {result}");
-
-                socket.Shutdown(SocketShutdown.Send);
+                socket.Shutdown(SocketShutdown.Send); //Đóng kết nối, không gửi dữ liệu nữa
 
                 Console.WriteLine($"Closing connection from {socket.RemoteEndPoint}\n\n");
                 Console.WriteLine("-----------------------------------------");
                 socket.Close();
 
+            //Xóa bộ đệm
                 Array.Clear(receiveBuffer, 0, size);
 
             }
